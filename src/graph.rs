c53f20560966b85fb7624917a0179887347c0391
evolp <128611555+evolp@users.noreply.github.com>
2025-04-07 11:11:43 +0200

    add complement(base) back, remove dir.flip()

--- src/graph.rs
+++ src/graph.rs
@@ -1634,13 +1634,13 @@ impl<K: Kmer, SD: SummaryData<u8> + Debug> Node<'_, K, SD>  {
 
         if let Some(em) = self.data().edge_mults() {
             let (b, dir) = if flipped { 
-                (base, incoming_dir.flip()) 
+                (complement(base), incoming_dir) 
             } else {
                 (base, incoming_dir)
             };
 
             let count = em.edge_mult(b, dir);
-            format!("[color={color}, label={count}]")
+            format!("[color={color}, label=\"{count}]\"")
         } else {
             format!("[color={color}]")
         }
