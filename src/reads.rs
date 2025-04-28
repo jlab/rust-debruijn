c1dfdcd5b0179303866838b416d9441d6ba146ab
evolp <128611555+evolp@users.noreply.github.com>
2025-04-28 17:45:25 +0200

    adapt ReadsPaired::tag_kmers to empty reads

--- src/reads.rs
+++ src/reads.rs
@@ -560,17 +560,22 @@ impl<D: Clone + Copy + Eq + Hash> ReadsPaired<D> {
 }
 
 impl ReadsPaired<u8> {
+    /// return the number of k-mers occuring with each u8-encoded tag, 
+    /// with the tag as the index
     pub fn tag_kmers(&self, k: usize) -> Vec<u64>{
         let hashed_kmer_counts = self.data_kmers(k);
 
-        let n_samples = hashed_kmer_counts.keys().max().expect("Error: no samples");
+        match hashed_kmer_counts.keys().max(){
+            Some(n_samples) => {
+                let mut kmer_counts = vec![0; *n_samples as usize + 1];
+                for (tag, kmer_count) in hashed_kmer_counts {
+                    kmer_counts[tag as usize] += kmer_count as u64;
+                }
 
-        let mut kmer_counts = vec![0; *n_samples as usize + 1];
-        for (tag, kmer_count) in hashed_kmer_counts {
-            kmer_counts[tag as usize] += kmer_count as u64;
+                kmer_counts
+            },
+            None => Vec::new()
         }
-
-        kmer_counts
     }
 }
 
