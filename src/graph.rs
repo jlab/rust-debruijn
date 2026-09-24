139c0f98250275a6636d7983330fae9f2c7cf905
evolp <128611555+evolp@users.noreply.github.com>
2026-09-24 17:43:19 +0200

    add debug to bubble dot path

--- src/graph.rs
+++ src/graph.rs
@@ -1954,6 +1954,7 @@ impl<K: Kmer, SD: Debug> DebruijnGraph<K, SD> {
                 if let (Some((colormap, config, translator)), true) = (write_info, ((avg_mgr.clone().sum::<f32>() > 0.) | (avg_sgr.clone().sum::<f32>() > 0.))) {
                     let nodes = paths.iter().flat_map(|vec| vec.iter().map(|a| a.0)).collect::<Vec<_>>();
                     let new_path = format!("{:?}/bubble-{bubbles_written}.dot", path.as_ref().parent().unwrap_or(Path::new(".")));
+                    debug!("path bubble dot: {:?}", new_path);
                     self.to_dot_partial(
                         new_path, 
                         &|node| node.node_dot_default(colormap, config, translator, false, false), 
